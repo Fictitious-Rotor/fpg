912f5b8b0a67bdae419b4ed64502f65cff6cb8ef
Fictitious-Rotor <fictitious.rotor@gmail.com>
2020-08-08 22:29:49 +0100

    Created a generic tokeniser. Created a lua implementation of the tokeniser

---
+++ luaTokens.lua
@@ -0,0 +1,205 @@
+local Tokeniser = require "tokeniser"
+local pattern = Tokeniser.pattern
+local Token = Tokeniser.makeToken
+
+--------------------------------------------------
+
+local strMatch = string.match
+local gsub = string.gsub
+
+--------------------------------------------------
+
+local function makeLuaPatternMatcher(lPat)
+  return function(str, startPos)
+    return strMatch(str, lPat, startPos) 
+  end
+end
+
+local function makeBasicMatcher(matcher, tokenMaker)
+  return function(str, startPos)
+    local match = matcher(str, startPos)
+  
+    if match then
+      return tokenMaker(match), startPos + #match
+    else
+      return false, startPos
+    end
+  end
+end
+
+local function escapeSymbols(str)
+  local outTbl = {}
+  
+  for char in str:gmatch('.') do
+    outTbl[#outTbl + 1] = '%'
+    outTbl[#outTbl + 1] = char
+  end  
+    
+  return table.concat(outTbl)
+end
+
+local function anchorPattern(str)
+  return '^' .. str
+end
+
+--------------------------------------------------
+
+local Identifier = Token "Identifier"
+local Whitespace = Token "Whitespace"
+local Number = Token "Number"
+
+local Keyword = Token "Keyword"
+local Symbol = Token "Symbol"
+
+local String = Token "String"
+local Comment = Token "Comment"
+
+--------------------------------------------------
+
+local matchWhitespace = makeBasicMatcher(makeLuaPatternMatcher("^%s+"), Whitespace)
+local matchIdentifier = makeBasicMatcher(makeLuaPatternMatcher("^[%a_][%w_]*"), Identifier)
+
+
+
+local function makeSymbolMatcher(aSymbol)
+  local escapedSymbols = escapeSymbols(aSymbol)
+  local matcher = makeLuaPatternMatcher(anchorPattern(escapedSymbols))
+  local token = Symbol(aSymbol)
+  
+  return makeBasicMatcher(matcher, function() return token end)
+end
+
+local function makeKeywordMatcher(aKeyword)
+  local matcher = makeLuaPatternMatcher(anchorPattern(aKeyword))
+  local token = Keyword(aKeyword)
+
+  return makeBasicMatcher(matcher, function() return token end)
+end
+
+
+
+local function matchNumber(aStr, startPos)
+  local readPos = startPos
+  local numberOpener = strMatch(aStr, "^%d+", readPos)
+  
+  if numberOpener then
+    readPos = readPos + #numberOpener
+    local outNum = numberOpener
+    
+    local decimalPoint = strMatch(aStr, "^%.", readPos)
+    
+    if decimalPoint then
+      readPos = readPos + #decimalPoint
+      outNum = outNum .. decimalPoint
+      
+      local numberCloser = strMatch(aStr, "^%d+", readPos)
+      
+      if numberCloser then
+        readPos = readPos + #numberCloser
+        outNum = outNum .. numberCloser
+      end
+    end
+    
+    return Number(outNum), readPos
+  end
+  
+  return false, startPos
+end
+
+local function matchString(aStr, startPos)
+  local stringOpener = strMatch(aStr, "^['\"]", startPos)
+  
+  if stringOpener then
+    local getContent = "^[^\n" .. stringOpener .. "]*"
+    local contentStartPos = startPos + #stringOpener
+  
+    local content = strMatch(aStr, getContent, contentStartPos)
+    
+    if not strMatch(aStr, stringOpener, contentStartPos + #content) then
+      error("Unterminated string!")
+    end
+    
+    return String(content), (startPos + #stringOpener + #content + #stringOpener)
+  else
+    local multilineOpener = strMatch(aStr, "^%[=*%[", startPos)
+    
+    if multilineOpener then
+      local multilineCloser = gsub(multilineOpener, "%[", "]")
+      local getContent = "(.-)" .. multilineCloser
+      local contentStartPos = startPos + #multilineOpener + 1
+      
+      local content = strMatch(aStr, getContent, contentStartPos)
+      
+      return String(content), (contentStartPos + #content + #multilineCloser)
+    end
+  end
+  
+  return false, startPos
+end
+
+local function matchComment(aStr, startPos)
+  local commentOpener = strMatch(aStr, "^%-%-", startPos)
+  
+  if commentOpener then
+    local multilineOpener = strMatch(aStr, "^%[=*%[", startPos + #commentOpener)
+    
+    if multilineOpener then -- Multiline comment
+      local multilineCloser = gsub(multilineOpener, "%[", "]")
+      local contentStart = startPos + #commentOpener + #multilineOpener
+      local getContent = "^(.-)" .. multilineCloser
+      local content = strMatch(aStr, getContent, contentStart)
+      
+      return Comment(content), (contentStart + #content)
+    else -- Inline comment
+      local contentStart = startPos + #commentOpener
+      local content = strMatch(aStr, "^[^\n]*", contentStart)
+      
+      return Comment(content), (contentStart + #content)
+    end
+  end
+  
+  return false, startPos
+end
+
+--------------------------------------------------
+
+local reservedSymbols = {
+  "]]", "[[", "]", "[", "::", "}", ")", '"', "...", "(", "\\", 
+  "{", "==", "~=", ";", "..", "=", "/", "%", "^", ",", ":", 
+  "-", "'", "*", "#", "+", "<=", ">=", ".", "<", ">" 
+}
+
+local reservedKeywords = { 
+  "do", "if", "in", "or", "end", "for", "nil", "and", "not", 
+  "else", "goto", "then", "true", "while", "until", "local", 
+  "break", "false", "repeat", "elseif", "return", "function", 
+}
+
+local allPatterns = {
+  pattern(matchString),
+  pattern(matchNumber),
+  pattern(matchComment, true),
+  pattern(matchWhitespace)
+}
+
+local staticTokens = { -- Convert all strings into tokens, then into patterns, then add them to allPatterns
+  { reservedSymbols, function(symbol) return pattern(makeSymbolMatcher(symbol)) end },
+  { reservedKeywords, function(keyword) return pattern(makeKeywordMatcher(keyword)) end },
+}
+
+for _, tokensAndMatcher in ipairs(staticTokens) do
+  local tokenSet = tokensAndMatcher[1]
+  local patternMaker = tokensAndMatcher[2]
+
+  for _, value in ipairs(tokenSet) do
+    allPatterns[#allPatterns + 1] = patternMaker(value)
+  end
+end
+
+allPatterns[#allPatterns + 1] = pattern(matchIdentifier) -- Identifier must be checked after keywords so that they have precedence
+
+--------------------------------------------------
+
+return function(str) 
+  return Tokeniser.lex(str, allPatterns)
+end
--- tokeniser.lua
+++ tokeniser.lua
@@ -1,161 +1,63 @@
 local view = require "debugview"
 
-local sMatch = string.match
-
-local function makeMatcher(lPat)
-  return function(str) return sMatch(str, lPat) end
-end
-
-local function arrContains(tbl, value)
-  for _, item in ipairs(tbl) do
-    if item == value then return true end
-  end
-  
-  return false
-end
-
-local function arrPartition(tbl, pred)
-  local trues, falses = {}, {}
-  local selectedTable
-
-  for _, val in ipairs(tbl) do
-    selectedTable = pred(val) and trues or falses
-    
-    selectedTable[#selectedTable + 1] = val
-  end
-  
-  return trues, falses
-end
-
----------
-
-local stringableMeta = { __tostring = function(tbl) return tbl.value end }
-
-local function stringableMaker(type)
-  return function(value)
-    return setmetatable({ value = value, type = type }, stringableMeta)
-  end
-end
-
-local whitespace = stringableMaker "whitespace"
-local identifier = stringableMaker "ident"
-
-function newIter(fn) -- Returns readFn & shouldReRead
-  local lastRead
-  local shouldReRead = false
-  
-  local function readFn()
-    if not shouldReRead then
-      lastRead = fn()
-      return lastRead
-    else
-      shouldReRead = false
-      return lastRead
-    end
-  end
-  
-  local function askReRead()
-    shouldReRead = true
-  end
-  
-  local function isEmpty()
-    return lastRead == nil
-  end
-  
-  return readFn, askReRead, isEmpty
-end
-
------------
-
-function consumeIdent(readChar, askReRead, canBeIdentifier)
-  local charTable = {}
-  local curChar = readChar()
-  
-  while canBeIdentifier(curChar)  do
-    charTable[#charTable + 1] = curChar
-    curChar = readChar()
-  end
-  
-  askReRead()
-  return table.concat(charTable), #charTable > 1
-end
-
-function consumeReservedStrings(readChar, askReRead, reservedStrings)
-  
-end
-
-local function makeMappingMatcher(mapping, elementKey)
-  return function(value)
-    for k,v in pairs(mapping) do
-      if v[elementKey](value) then
-        return true
-      end
-    end
-    
-    return false
-  end
-end
-
--- sort by "canBeIdentifier"
--- Which allows me to group into keywords & symbols
--- perhaps "reservedIdents" and "reservedStrings"
-
-
-
--- YOU FORGOT ABOUT STRINGS!!
-
 local Tokeniser = {}
-Tokeniser.__index = Tokeniser
 
--- Put "LPat corresponds to Lua's patterns" in the documentation somewhere appropriate
--- Put something about shortening "language construct" to LC
-local function Tokeniser.new(isWhitespaceLPat, languageConstructMapping, allReserved)
-  local reservedLCs, reservedStrings = arrPartition(allReserved, makeMappingMatcher(languageConstructMapping, "isMatch"))
-  local sortedReserved = table.sort(reservedStrings)
-
-  return setmetatable({ 
-    isWhitespaceChar = isWhitespaceChar, 
-    reservedLCs = reservedLCs,
-    reservedStrings = sortedReserved
-  }, Tokeniser)
+function Struct(propertyNames)
+  return function(...)
+    local theStruct = {}
+    local args = { ... }
+    
+    for idx, propertyName in ipairs(propertyNames) do
+      theStruct[propertyName] = args[idx]
+    end
+    
+    return theStruct
+  end
 end
 
-function Tokeniser.readString(inputString, isWhitespaceChar, isIdentifierChar, reservedLCs, reservedStrings)
-  local readChar, askReRead, isEmpty = newIter(inputString:gmatch("."))
-  
-  local outTokens = {}
-  local canTokenise = true
-  local found, succeeded, isReserved
-  
-  repeat
-    found, succeeded = consumeWhitespace()
+Tokeniser.pattern = Struct { "match", "isPriority" }
+
+local token = Struct { "type", "content" }
+
+function Tokeniser.makeToken(type)
+  return function(content)
+    return token(type, content)
+  end
+end
+
+local function scan(str, startPos, patterns)
+  local bestMatch, furthestPos = false, startPos
+
+  for _, pattern in pairs(patterns) do
+    local match, endPos = pattern.match(str, startPos)
     
-    if succeeded then
-      outTokens[#outTokens + 1] = found
+    if match then
+      if endPos > furthestPos then
+        bestMatch = match
+        furthestPos = endPos
+      elseif pattern.isPriority then
+        return match, endPos
+      end 
     end
-    
-    found, succeeded = consumeIdentifier(readChar, askReRead)
-    
-    if succeeded then
-      isReserved = arrContains(reservedLCs, found)
-      
-      outTokens[#outTokens + 1] = isReserved and found or identifier(found)
-    else
-      found, succeeded = consumeReservedString(readChar, askReRead)
-      
-      if succeeded then
-        outTokens[#outTokens + 1] = found
-      else
-        canTokenise = false
-      end
-    end
-  until not canTokenise or isEmpty()
-  
-  if not canTokenise and isEmpty() then
-    error(string.format("Tokeniser error: failed to tokenise the entire string.\nSuccessfully lexed tokens are as follows:\n%s", view(outTokens)))
   end
   
-  return outTokens
+  return bestMatch, furthestPos
 end
 
-return Tokeniser.new
+function Tokeniser.lex(str, patterns)
+  local foundTokens, pos, token = {}, 1, false
+  
+  while pos <= #str do
+    token, pos = scan(str, pos, patterns)
+    
+    if token then
+      foundTokens[#foundTokens + 1] = token
+    else
+      error(string.format("Tokeniser error: failed to tokenise the entire string.\nSuccessfully lexed tokens are as follows:\n%s", view(foundTokens)))
+    end
+  end
+  
+  return foundTokens
+end
+
+return Tokeniser
